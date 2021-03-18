9b595165b78a28aa5ceb1a772bd3d64322f1fb3c
anuj.bagerwal <anuj.bagerwal@green-apex.com>
2021-03-18 16:46:11 +0530

    comment home btn

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,7 +9,7 @@ const Nav = ({ toggleNewToggle, Reset, responseGoogle, userName, Logout }) => {
   return (
     <div className='flex  absolute top-0 bg-blue-100 w-full p-3'>
       <div className='flex '>
-        <span onClick={() => Reset()}>
+        {/* <span onClick={() => Reset()}>
           <svg
             className='w-10 h-10 mr-10 ml-4 '
             fill='none'
@@ -24,7 +24,7 @@ const Nav = ({ toggleNewToggle, Reset, responseGoogle, userName, Logout }) => {
               d='M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6'
             ></path>
           </svg>
-        </span>
+        </span> */}
         <span
           onClick={() => setTheme(colorTheme)}
           className='w-10 h-10 bg-indigo-500 block rounded-full shadow-lg 
